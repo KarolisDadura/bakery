8ee071518371ccdfd224b68f7f0d70257aa69c1e
KarolisDadura <33262517+KarolisDadura@users.noreply.github.com>
2017-11-09 20:33:53 +0200

    20.33

--- index.php
+++ index.php
@@ -96,18 +96,56 @@
 			<td>2</td>
 		</tr>	
 	<script type="text/javascript">
-		
-		{
-			date: "2017-11-09",
-			product: "aguoninė",
-			data: 
-		}
-
-		[5,20,19,4,2]
 
 		var Pavadinimas = ["aguoninė", "varškės", "duona balta", "duona juoda", "duona balta", "duona ruginė"];
-		var data =
-		var 
+		
+		// Option I - unsorted
+
+		var _arrayofObjects = [
+			{
+				"date": "2017-11-09",
+				"product": "aguoninė",
+				"data:": [5,20,19,4,2],
+			}
+		// Option II - sort by date
+
+		var _objectByDay = {
+			"2017-11-09":
+		
+			{
+				"date": "2017-11-09",
+				"product": "aguoninė",
+				"data:": [5,20,19,4,2],
+			}
+
+		// Option III - objects by product
+			var _ObjectsByProduct = {
+				"varškės":
+				[
+				{
+					"date": "2017-11-09",
+					"data:": [3,28,25,2,4]
+				},
+				{
+					"date": "2017-11-10",
+					"data:": [3,28,25,2,4]
+				}
+				],
+				"aguoninė":
+				[
+				{
+					"date": "2017-11-09",
+					"data:": [5,20,19,4,2],
+				},
+				{
+					"date": "2017-11-10",
+					"data:": [5,20,19,4,2],
+				}
+				],
+
+		// Option IV - optimized product
+			
+
 			
 	</script>	
 			
