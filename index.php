746c7c00673c158949ec66f3e4c92473d7a935b2
KarolisDadura <33262517+KarolisDadura@users.noreply.github.com>
2017-11-09 19:37:39 +0200

    kode duomenys iki begalybes

--- index.php
+++ index.php
@@ -11,8 +11,14 @@
 		<tr>
 		<th rowspan="2">Pavadinimas</th>
 		<th colspan="5">2017-11-09</th>
+		<th colspan="5">2017-11-10</th>
 		</tr>
 		<tr>
+		<th>VL</th> <!-- Vakrykštis likutis-->
+		<th>PG</th> <!-- Pagaminta-->
+		<th>PR</th> <!-- Parduota-->
+		<th>SG</th> <!-- Sugadinta-->
+		<th>GL</th> <!-- Galutinis likutis-->
 		<th>VL</th> <!-- Vakrykštis likutis-->
 		<th>PG</th> <!-- Pagaminta-->
 		<th>PR</th> <!-- Parduota-->
@@ -30,9 +36,20 @@
 			<td>19</td>
 			<td>4</td>
 			<td>2</td>
+			<td>5</td>
+			<td>20</td>
+			<td>19</td>
+			<td>4</td>
+			<td>2</td>
+
 		</tr>
 		<tr>
 			<td>varškės</td>
+			<td>3</td>
+			<td>28</td>
+			<td>25</td>
+			<td>2</td>
+			<td>4</td>
 			<td>3</td>
 			<td>28</td>
 			<td>25</td>
@@ -46,6 +63,11 @@
 			<td>15</td>
 			<td>0</td>
 			<td>3</td>
+			<td>1</td>
+			<td>17</td>
+			<td>15</td>
+			<td>0</td>
+			<td>3</td>
 		</tr>	
 		<tr>
 			<td>duona juoda</td>
@@ -54,9 +76,19 @@
 			<td>50</td>
 			<td>0</td>
 			<td>0</td>
+			<td>0</td>
+			<td>50</td>
+			<td>50</td>
+			<td>0</td>
+			<td>0</td>
 		</tr>
 		<tr>
 			<td>duona ruginė</td>
+			<td>2</td>
+			<td>18</td>
+			<td>16</td>
+			<td>2</td>
+			<td>2</td>
 			<td>2</td>
 			<td>18</td>
 			<td>16</td>
