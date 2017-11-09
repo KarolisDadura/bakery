fb2cc9a22adb144f5a7af307f24e937c0a60c989
KarolisDadura <33262517+KarolisDadura@users.noreply.github.com>
2017-11-09 19:33:53 +0200

    suvesti dumenys ranka

--- index.php
+++ index.php
@@ -25,18 +25,43 @@
 	<tbody>
 		<tr>
 			<td>aguoninė</td>
+			<td>5</td>
+			<td>20</td>
+			<td>19</td>
+			<td>4</td>
+			<td>2</td>
 		</tr>
 		<tr>
 			<td>varškės</td>
+			<td>3</td>
+			<td>28</td>
+			<td>25</td>
+			<td>2</td>
+			<td>4</td>
 		</tr>	
 		<tr>
 			<td>duona balta</td>
+			<td>1</td>
+			<td>17</td>
+			<td>15</td>
+			<td>0</td>
+			<td>3</td>
 		</tr>	
 		<tr>
 			<td>duona juoda</td>
+			<td>0</td>
+			<td>50</td>
+			<td>50</td>
+			<td>0</td>
+			<td>0</td>
 		</tr>
 		<tr>
 			<td>duona ruginė</td>
+			<td>2</td>
+			<td>18</td>
+			<td>16</td>
+			<td>2</td>
+			<td>2</td>
 		</tr>	
 			
 	</tbody>
