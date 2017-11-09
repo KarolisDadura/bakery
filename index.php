ff35cf5dd3623ee0964236123e0a4968229b1bbe
KarolisDadura <33262517+KarolisDadura@users.noreply.github.com>
2017-11-09 19:24:57 +0200

    bakery

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bakery</title>
+	<link rel="stylesheet" type="text/css" href="app.css">
+</head>
+<body>
+
+<table border>
+	<thead>
+		<tr>
+		<th rowspan="2">Pavadinimas</th>
+		<th colspan="5">2017-11-09</th>
+		</tr>
+		<tr>
+		<th>VL</th> <!-- Vakrykštis likutis-->
+		<th>PG</th> <!-- Pagaminta-->
+		<th>PR</th> <!-- Parduota-->
+		<th>SG</th> <!-- Sugadinta-->
+		<th>GL</th> <!-- Galutinis likutis-->
+	
+		</tr>
+	</thead>
+
+	<tbody>
+		<tr>
+			<td>aguoninė</td>
+		</tr>
+		<tr>
+			<td>varškės</td>
+		</tr>	
+		<tr>
+			<td>duona balta</td>
+		</tr>	
+		<tr>
+			<td>duona juoda</td>
+		</tr>
+		<tr>
+			<td>duona ruginė</td>
+		</tr>	
+			
+	</tbody>
+
+</table>	
+
+</body>
+</html>
