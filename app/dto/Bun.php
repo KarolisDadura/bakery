93067d8d9e4c0f82a21182b2cd9c68417810f349
KarolisDadura <33262517+KarolisDadura@users.noreply.github.com>
2017-12-04 20:10:10 +0200

    class Bun

--- app/dto/Bun.php
+++ app/dto/Bun.php
@@ -2,19 +2,11 @@
 
 class Bun
 {
-	public function __construct (string $key)
-	{
-		echo "hello" . $key . "\r\n";
-	}
+	public $name;
+	public $id;
+	public $SalesPrice;
+	public $Weight;
+	public $ean;
 }
 
-$bun = new Bun('a');
-$bun = new Bun('b');
-$bun = new Bun('c');
-$bun = new Bun('d');
-$bun = new Bun('e');
-$bun = new Bun('f');
-$bun = new Bun('g');
-$bun = new Bun('h');
-$bun = new Bun('i');
-$bun = new Bun('j');
+$bun = new Bun();
