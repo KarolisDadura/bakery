e5642091c27a2bee744605cd8e116d68732a0540
KarolisDadura <33262517+KarolisDadura@users.noreply.github.com>
2017-12-04 20:00:25 +0200

    klases

--- app/dto/Bun.php
+++ app/dto/Bun.php
@@ -4,12 +4,15 @@
 class Bun
 {
 	const OWNER = "Bakery Inc.";
+	public static $OWNER_TWO = "UAB Kepykla";
 	private $a = "A";
 	protected $b = "B";
 	public $c = "C";
 }
 
+Bun::$OWNER_TWO = Bun::OWNER;
 print_r(Bun::OWNER);
+print_r(Bun::$OWNER_TWO);
 die ();
 $bun = new Bun();
 
