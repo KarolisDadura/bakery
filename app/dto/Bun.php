0d69745cf06c24ce205b1e5b1248733d83839b7c
KarolisDadura <33262517+KarolisDadura@users.noreply.github.com>
2017-12-04 19:52:54 +0200

    pasiekti konstanta su  ::

--- app/dto/Bun.php
+++ app/dto/Bun.php
@@ -3,12 +3,16 @@
 
 class Bun
 {
+	const OWNER = "Bakery Inc.";
 	private $a = "A";
 	protected $b = "B";
 	public $c = "C";
 }
 
+print_r(Bun::OWNER);
+die ();
 $bun = new Bun();
+
 
 print_r($bun);
 print_r($bun ->c);
